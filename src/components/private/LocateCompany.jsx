c63e9ad286a6dd72295723d5d2d318d945ff28a6
Faree99 <adebayofaridah99@gmail.com>
2024-12-07 10:24:56 +0100

    Updated

--- src/components/private/LocateCompany.jsx
+++ src/components/private/LocateCompany.jsx
@@ -233,13 +233,13 @@ const LocateCompany = () => {
           </div>
   
           <nav className="flex flex-col p-4">
-            <Link to="/dashboard" className="py-2 text-white hover:bg-green-300" onClick={closeSidebar}>Home</Link>
-            <Link to="/requestpickup" className="py-2 text-white hover:bg-green-300" onClick={closeSidebar}>Request Pickup</Link>
-            <Link to="/locatecompany" className="py-2 text-white hover:bg-green-300" onClick={closeSidebar}>Locate Company</Link>
-            {/* <Link to="/my-services" className="py-2 text-white hover:bg-green-300" onClick={closeSidebar}>My Services</Link> */}
-            <Link to="/payment-history" className="py-2 text-white hover:bg-green-300" onClick={closeSidebar}>Payment History</Link>
-            <Link to="/profile" className="py-2 text-white hover:bg-green-300" onClick={closeSidebar}>Profile</Link>
-            <Link to="/manage-account" className="py-2 text-white hover:bg-green-300" onClick={closeSidebar}>Manage Account</Link>
+            <Link to="/dashboard" className="py-2 text-white hover:bg-green-500" onClick={closeSidebar}>Home</Link>
+            <Link to="/requestpickup" className="py-2 text-white hover:bg-green-500" onClick={closeSidebar}>Request Pickup</Link>
+            <Link to="/locatecompany" className="py-2 text-white hover:bg-green-500" onClick={closeSidebar}>Locate Company</Link>
+            {/* <Link to="/my-services" className="py-2 text-white hover:bg-green-500" onClick={closeSidebar}>My Services</Link> */}
+            <Link to="/payment-history" className="py-2 text-white hover:bg-green-500" onClick={closeSidebar}>Payment History</Link>
+            <Link to="/profile" className="py-2 text-white hover:bg-green-500" onClick={closeSidebar}>Profile</Link>
+            <Link to="/manage-account" className="py-2 text-white hover:bg-green-500" onClick={closeSidebar}>Manage Account</Link>
             <Link to="/private-sector" className="py-2 text-red-600 hover:text-red-800 " onClick={closeSidebar}>Logout</Link>
           </nav>
         </aside>
